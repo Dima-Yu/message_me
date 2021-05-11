366984f21a432306a64d367ac07cff3b085ed191
Dima-Yu <mindwork.one@gmail.com>
2021-05-11 16:57:54 +0300

    add messages from ui functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,15 @@
-class MessagesController < ApplicateionController
+class MessagesController < ApplicationController
+  before_action :require_user
+  def create
+    message = current_user.messages.build(msg_params) 
+    if message.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def msg_params
+    params.require(:message).permit(:body)
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
+
   def new; end
 
   def create
@@ -17,5 +19,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'You are logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are logged in"
+      redirect_to root_path
+    end
   end
 end
